39b04051eac4b92374cf9b86d1f377d455e4d590
Anders Fugmann <anders@fugmann.net>
2022-09-19 22:00:54 +0200

    Use confirm to ensure message have been delivered to the queue

--- test/queue_test.ml
+++ test/queue_test.ml
@@ -5,22 +5,24 @@ let port =
   Sys.getenv_opt "AMQP_PORT"
   |> Option.map int_of_string
 
+
+let assert_ok = function
+  | `Ok -> ()
+  | `Rejected -> failwith "Message rejected"
+
 let test_amqp env =
   Eio.Switch.run (fun sw ->
     let connection = Connection.init ~sw ~env ~id:"Test" ?port "127.0.0.1" in
     Eio.traceln "Connection created";
-    let channel = Channel.init ~sw connection Channel.no_confirm in
+    let channel = Channel.init ~sw connection Channel.with_confirm in
     Eio.traceln "Channel created";
     let queue = Queue.declare channel "test_queue" in
     Eio.traceln "Queue created";
     let _purge = Queue.purge queue channel in
     (* Lets create a couple of message *)
-    Queue.publish queue channel (Message.make "Msg1");
-    Queue.publish queue channel (Message.make "Msg2");
-    Queue.publish queue channel (Message.make "Msg3");
-
-    (* Need to ensure that all message are flushed *)
-    Eio.Time.sleep (Eio.Stdenv.clock env) 1.0;
+    Queue.publish queue channel (Message.make "Msg1") |> assert_ok;
+    Queue.publish queue channel (Message.make "Msg2") |> assert_ok;
+    Queue.publish queue channel (Message.make "Msg3") |> assert_ok;
 
     let deleted_messages = Queue.delete queue channel in
     Eio.traceln "Queue deleted with %d messages" deleted_messages;
